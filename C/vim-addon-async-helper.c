a87649337a9ad8ccafbdf220db2080f971036df3
Marc Weber <marco-oweber@gmx.de>
2010-10-29 05:02:17 +0200

    this could be the initial release (?). Not perfect yet. But I think its already quite usable

---
+++ C/vim-addon-async-helper.c
@@ -0,0 +1,324 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <error.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+ // wait 50ms before polling again
+#define TOWAIT 20
+#define BUF_SIZE 4048
+
+/* author: Marc Weber
+ * license: same as Vim .c code
+ *
+ * purpose: helper app providing one async implementation for vim-addon-async.
+ * from-vim-file: Vim will write to this file. The loop will pick it up and feed
+ * it into stdin of the process.
+ *
+ * to-vim-file
+ *
+ * If stdout/err text is received the data is stored as serialized Vim string
+ * (so that \n \r are preserved). Then a remote call is into Vim is done to
+ * make it read and process it
+ *
+ * I'm not a C programmer. So this code may contain bugs. Bear with me and help
+ * me improve it. But hey, it works!
+ *
+ * Qute some code was copied from vim source code
+ */ 
+
+void usage(char * name){
+  printf("%s vim-executable vim-server-name process_id from-vim-file to-vim-file cmd\n", name);
+}
+
+static void
+simulate_dumb_terminal()
+{
+
+    int Columns = 80;
+    int Rows = 24;
+
+# ifdef HAVE_SETENV
+    char	envbuf[50];
+    setenv("TERM", "dumb", 1);
+    sprintf((char *)envbuf, "%ld", Rows);
+    setenv("ROWS", (char *)envbuf, 1);
+    sprintf((char *)envbuf, "%ld", Rows);
+    setenv("LINES", (char *)envbuf, 1);
+    sprintf((char *)envbuf, "%ld", Columns);
+    setenv("COLUMNS", (char *)envbuf, 1);
+# else
+    static char	envbuf_Rows[20];
+    static char	envbuf_Columns[20];
+    /*
+     * Putenv does not copy the string, it has to remain valid.
+     * Use a static array to avoid losing allocated memory.
+     */
+    putenv("TERM=dumb");
+    sprintf(envbuf_Rows, "ROWS=%ld", Rows);
+    putenv(envbuf_Rows);
+    sprintf(envbuf_Rows, "LINES=%ld", Rows);
+    putenv(envbuf_Rows);
+    sprintf(envbuf_Columns, "COLUMNS=%ld", Columns);
+    putenv(envbuf_Columns);
+# endif
+}
+
+// target should be strlen(from) * 2 + 2
+void vimQuote(char * ptr, char * target){
+
+  *target++ = '\"';
+  while (*ptr){
+    switch (*ptr){
+      case '\\' :  
+        *target++ = '\\';
+        *target++ = '\\';
+        break;
+      case '\n' :  
+        *target++ = '\\';
+        *target++ = 'n';
+        break;
+      case '\r' :  
+        *target++ = '\\';
+        *target++ = 'r';
+        break;
+      case '"' :  
+        *target++ = '\\';
+        *target++ = '"';
+        break;
+      default :
+        *target++ = *ptr;
+    }
+    ptr++;
+  }
+  *target++ = '\"';
+  *target++ = 0;
+}
+
+void send(char * vimExecutable, char *vimServerName, char * processId, char * buf, int read_bytes){
+  char command[2*BUF_SIZE+1];
+  char quoted[2*BUF_SIZE+3];
+
+  vimQuote(buf, quoted);
+  snprintf(command, sizeof(command), "<esc>:call async#Receive(\"%s\", %s)<cr>", processId, quoted);
+
+  char * argv3[6];
+  argv3[0] = vimExecutable;
+  argv3[1] = "--servername";
+  argv3[2] = vimServerName;
+  argv3[3] = "--remote-send";
+  printf("command is %s\n", command);
+  argv3[4] = command;
+  argv3[5] = NULL;
+
+  int i;
+  for (i = 0; i < 5; i++) {
+    printf("arg %d %s\n", i, argv3[i]);
+  }
+
+  int pid2;
+
+  pid2 = fork();
+  if (pid2 == -1) {
+    printf("fork failed\n");
+  } else if (pid2 == 0) { /* child */
+    execvp(vimExecutable, argv3);
+    exit(-1);
+  }
+  // parent waits for sending data:
+  int status;
+  while (1){
+    waitpid(pid2, &status, 0);
+    if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+      printf("sending failed!\n");
+    if (WIFEXITED(status))
+      break;
+  }
+}
+
+int main(int argc, char * argv[])
+{
+
+  int i;
+  if (argc < 7) {
+    usage(argv[0]);
+  } else {
+    argv++; argc--;
+    char * vimExecutable = argv[0];
+    char * vimServerName = argv[1];
+    char * processId     = argv[2];
+    char * inputFile     = argv[3]; // from Vim to this tool
+    char * writeFile     = argv[4]; // stdout to Vim
+    char * cmd           = argv[5];
+    int    pipe_error = 0;
+
+
+    // this is mostly copied from vim code (mch_start_async_shell)
+    int	fd_fromshell[2];
+    int	fd_toshell[2];
+    int fd_from;
+    int fd_to;
+    int pid;
+    int ignored;
+
+
+    // create communication pipes:
+    pipe_error = (pipe(fd_fromshell) < 0);
+    if (pipe_error) {
+	printf("creating pipe failed\n");
+	goto error_pipe_from;
+    }
+    pipe_error = (pipe(fd_toshell) < 0);
+    if (pipe_error) {
+	printf("creating pipe failed\n");
+	goto error_pipe_to;
+    }
+
+    // fork and close pipe ends
+    pid = fork();
+    if (pid == -1) {
+	printf("fork failed\n");
+	goto error_fork;
+
+    } else if (pid == 0) { /* child */
+	simulate_dumb_terminal();
+
+	/* set up stdin for the child */
+	close(fd_toshell[1]);
+	close(0);
+	ignored = dup(fd_toshell[0]);
+	close(fd_toshell[0]);
+
+	/* set up stdout/stderr for the child */
+	close(fd_fromshell[0]);
+	close(1);
+	ignored = dup(fd_fromshell[1]);
+	close(2);
+	ignored = dup(fd_fromshell[1]);
+	close(fd_fromshell[1]);
+
+        if (0 != system(cmd)){
+          printf("starting cmd %s failed\n", cmd);
+          exit(1);
+        } else {
+          exit(0);
+        }
+    }
+
+    /* parent */
+    close(fd_fromshell[1]);
+    close(fd_toshell[0]);
+    fd_from = fd_fromshell[0];
+    fd_to   = fd_toshell[1];
+
+
+    // set non blocking:
+
+    // notify Vim about pid:
+    char s_pid[10];
+    snprintf(&s_pid[0], sizeof(s_pid), "p%d", pid);
+    send(vimExecutable, vimServerName, processId, s_pid, strlen(s_pid));
+
+    // poll / select loop
+
+     int read_bytes;
+     char buf[BUF_SIZE];
+
+
+
+     while (1){
+       FILE * f_input = fopen(inputFile, "r");
+
+       // check for input .. probably I should be creating a fifo ..
+       if (f_input != NULL) {
+         printf("got file from vim \n");
+         read_bytes = fread(&buf, 1, BUF_SIZE-1, f_input);
+         buf[read_bytes] = 0;
+         printf("got bytes: %d \n%s\n", read_bytes, &buf[0]);
+         size_t written = write(fd_to, &buf[0], read_bytes);
+         if (written != read_bytes) printf("failed writing all bytes\n");
+         fclose(f_input);
+         unlink(inputFile);
+       }
+
+       fd_set		rfds, efds;
+
+       FD_ZERO(&rfds); /* calls bzero() on a sun */
+       FD_ZERO(&efds);
+
+       FD_SET(fd_from, &rfds);
+       FD_SET(fd_from, &efds);
+
+       struct timeval  tv;
+       struct timeval	*tvp;
+
+       tv.tv_sec = TOWAIT / 1000;
+       tv.tv_usec = (TOWAIT % 1000) * (1000000/1000);
+
+       int ret = select(fd_from +1, &rfds, NULL, &efds, &tv);
+       if (ret > 0){
+         printf("got data\n");
+         ret--;
+         // read_bytes and send buf to Vim
+
+         read_bytes = read(fd_from, buf, BUF_SIZE-1);
+         buf[read_bytes] = 0;
+         printf("sending bytes: %s\n", buf);
+         char quoted[2*BUF_SIZE+3];
+         // serialize as string because Vim can't preserve \n \r when reading :-(
+         vimQuote(buf, quoted);
+
+         FILE * out = fopen(writeFile, "w+");
+         fwrite(&quoted, strlen(quoted), 1, out);
+         fclose(out);
+         printf("file for vim written, calling vim\n");
+         send(vimExecutable, vimServerName, processId, "d", read_bytes + 1);
+         // wait until Vim has read the data
+         
+         printf("waiting for vim to remove file\n");
+         while (out = fopen(writeFile, "r")){
+           fclose(out);
+           // abuse select for sleeping
+           int ret = select(0, NULL, NULL, NULL, &tv);
+         }
+         printf("waiting finished\n");
+
+         if (read_bytes == 0)
+           goto terminate;
+       }  else {
+         // char b_[21];
+         // read(fd_from, b_, 20);
+         // buf[20] = 0;
+         // printf("read: %s\n", b_);
+       }
+     }
+
+    int status;
+    int rc;
+terminate:
+
+    if ( waitpid(pid, &status, WNOHANG) == 0 ) {
+        // comments see vim code
+        kill(pid, SIGTERM);
+        rc = waitpid(pid, &status, 0);
+    }
+
+    char code[8];
+    snprintf(&code[0], sizeof(code), "k%d", status);
+    send(vimExecutable, vimServerName, processId, code, strlen(code));
+
+error_fork:
+     close(fd_toshell[0]);
+     close(fd_toshell[1]);
+error_pipe_to:
+     close(fd_fromshell[0]);
+     close(fd_fromshell[1]);
+error_pipe_from:
+     return;
+  }
+
+}
--- autoload/async.vim
+++ autoload/async.vim
@@ -12,7 +12,7 @@ let s:impl = {'supported': 'no'}
 
 " tool {{{ 1
 
-fun! async#AppendBuffer(bnr, lines)
+fun! async#AppendBuffer(bnr, lines, moveLast)
   if 0 && exists('*withcurrentbuffer')
     call withcurrentbuffer(a:bnr, function('append'), [a:lines])
    else
@@ -20,6 +20,9 @@ fun! async#AppendBuffer(bnr, lines)
      sp
      exec 'b '.a:bnr
      call append('$', a:lines)
+     if a:moveLast
+       exec "normal G"
+     endif
      q!
    endif
 endf
@@ -54,8 +57,8 @@ endf
 "   call async#LogToBuffer({'cmd':'sh -c "es(){ echo -n \$1; sleep 1; }; while read f; do echo \$f; es a; es b; es c; echo; done"', 'move_last':1})
 fun! async#LogToBuffer(ctx)
   let ctx = a:ctx
-  let ctx['bufnr'] = bufnr('%')
-  sp
+  sp | enew
+  let ctx.bufnr = bufnr('%')
   let b:ctx = ctx
   let prefix = '> '
   exec 'noremap <buffer> o o'.prefix
@@ -64,7 +67,7 @@ fun! async#LogToBuffer(ctx)
   exec 'noremap <buffer> <space><cr> :call<space>async#Write(b:ctx, async#GetLines(''^'.prefix.'\zs.*\ze'')."\n")<cr>'
   exec 'inoremap <buffer> <space><cr> <esc>:call<space>async#Write(b:ctx, async#GetLines(''^'.prefix.'\zs.*\ze'')."\n")<cr>'
   fun! ctx.started()
-    call async#AppendBuffer(self.bufnr, "pid: " .self.pid)
+    call async#AppendBuffer(self.bufnr, "pid: " .self.pid, 1)
   endf
   fun! ctx.receive(type, text)
     try
@@ -83,6 +86,7 @@ fun! async#LogToBuffer(ctx)
       endif
       if lines[-1] == '' || (has_key(self, 'prompt') &&  lines[-1] =~ self.prompt)
         " trailing \n or prompt. drop it
+        silent! unlet self.pending
         call remove(lines, -1)
       else
         " no trailing \n. Assume continuation will be sent in next block
@@ -92,20 +96,21 @@ fun! async#LogToBuffer(ctx)
 
       " let lines = map(lines, string('<: ').'.v:val')
       let lines = filter(lines, 'v:val != ""')
-      " call async#AppendBuffer(self.bufnr, lines)
       if has_key(self,'regex_drop')
         let lines = filter(lines, 'v:val !~'.string(self.regex_drop))
       endif
-      call append('$', lines)
-      if has_key(self,'move_last')
-        exec "normal G"
-      endif
+      " call append('$', lines)
+      "
+      " if has_key(self, 'move_last'))
+        " exec "normal G"
+      " endif
+      call async#AppendBuffer(self.bufnr, lines, has_key(self, 'move_last'))
     catch /.*/
       call append('$',v:exception)
     endtry
   endf
   fun! ctx.terminated()
-    call async#AppendBuffer(self.bufnr, "exit code: ". self.status)
+    call async#AppendBuffer(self.bufnr, "exit code: ". self.status, 1)
   endf
   call async#Exec(ctx)
   return ctx
@@ -114,17 +119,62 @@ endf
 " }}}
 
 " implementation {{{ 1
+
 fun! s:Select(name, p)
   return (a:p && g:async_implementation == 'auto') || a:name == g:async_implementation
 endf
 
-if s:Select('native', exists('*async_exec'))
-
+let s:async_helper_path = fnamemodify(expand('<sfile>'),':h:h').'/C/vim-addon-async-helper'
+if s:Select('native', exists('*async_exec')) && !has('gui') 
   " Vim async impl
   for i in ['exec','kill','write','list']
     let s:impl[i] = function('async_'.i)
   endfor
   let s:impl['supported'] = 'native'
+
+elseif s:Select('c_executable', 1) && executable(s:async_helper_path)
+
+  " client-server callback is async#Receive
+  let s:processes = {}
+  let s:process_id = 1
+
+  fun! s:impl.exec(ctx)
+    let s:processes[s:process_id] = a:ctx
+    " input_file2 will be moved to input_file
+    " stdout_file is used to pass data back to Vim after async#Receive
+    " notification
+    let a:ctx.tmp_from_vim = tempname()
+    let a:ctx.tmp_from_vim2 = tempname()
+    let a:ctx.tmp_to_vim = tempname()
+    " start background process
+    let cmd = s:async_helper_path.' vim '.join(map([v:servername, s:process_id, a:ctx.tmp_from_vim, a:ctx.tmp_to_vim, a:ctx.cmd], 'shellescape(v:val)'),' ').'&'
+    " let g:cmd = cmd
+    call system(cmd)
+    let s:process_id += 1
+  endf
+  fun! s:impl.kill(ctx)
+    exec '!pkill '. a:ctx.pid
+  endf
+  fun! s:impl.write(ctx, input)
+    if  (-1 == writefile(split(a:input,"\n",1), a:ctx.tmp_from_vim2, 'b'))
+      echoe "writing vim to  tool file failed!"
+    endif
+    " mv so that its an atomic operation (make sure the process does't read a
+    " half written file)
+    call rename(a:ctx.tmp_from_vim2, a:ctx.tmp_from_vim)
+    " wait until the proces removed the file
+    let start_waiting = localtime()
+    while (filereadable(a:ctx.tmp_from_vim))
+      if localtime() - start_waiting > 10
+        echoe "external helper process didn't read the input file !"
+        " vim will clean up tmp file on shutdown
+        return
+      endif
+    endw
+  endf
+  fun! s:impl.list()
+    return s:processes
+  endf
 
 elseif s:Select('mzscheme', has('mzscheme'))
 
@@ -134,11 +184,29 @@ elseif s:Select('python', has('python'))
 
   " TODO python implementation calling back into vim using client-server
 
-elseif s:Select('c_executable', 0)
-
-  " TODO external c helper app implementation calling back into vim using client-server
-
 endif
+
+fun! async#Receive(processId, data)
+  if !has_key(s:processes, a:processId)
+    echoe "async#Receive called with unkown vim process identifier"
+    return
+  endif
+  let ctx = s:processes[a:processId]
+  let message = a:data[:0]
+  let data = a:data[1:]
+  if message == "p"
+    let ctx.pid = 1 * data
+    call ctx.started()
+  elseif message == "d"
+    " eval es evil .. but its the only way to preserve \r \n in input
+    " because a tempname is used I think its ok
+    call ctx.receive("stdout", eval(readfile(ctx.tmp_to_vim,'b')[0]))
+    call delete(ctx.tmp_to_vim)
+  elseif message == "k"
+    let ctx.status = 1 * data
+    call ctx.terminated()
+  endif
+endf
 
 " documentation see README
 
@@ -159,8 +227,8 @@ fun! async#Write(ctx, input)
   return s:impl.write(a:ctx, a:input)
 endf
 
-fun! async#List(ctx)
-  return s:impl.list(a:ctx)
+fun! async#List()
+  return s:impl.list()
 endf
 "
 " does this make sense?
